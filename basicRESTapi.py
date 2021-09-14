d9f01c2d1281a36d3d45d4c4a4d14610e1a09577
mark <89848850+wottreng@users.noreply.github.com>
2021-09-13 20:08:55 -0400

    Create basicRESTapi.py

---
+++ basicRESTapi.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify, make_response
+import config
+
+import logging
+
+app = Flask(__name__)
+app.config.from_object(config)
+
+
+@app.route("/api", methods=['GET', 'POST'])
+def myAPI():
+    print(request)
+    if request.method == 'POST':
+        print(request.form)  # form data
+        print(request.data)  # json data
+        return make_response(jsonify({"response": "post request received"}), 200)
+    else:
+        print(request.args)  # ex. http://localhost:8080/api?test=1
+        return make_response(jsonify({"response": "get request received"}), 200)
+
+
+# ======================================
+if __name__ == '__main__':
+    print('basic REST api enabled on localhost port 8080')
+    app.run(debug=True, host='127.0.0.1', port=8080)
