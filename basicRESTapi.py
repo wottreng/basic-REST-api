1506099d673ad0059cc70d121e0bdc344b03621d
mark <89848850+wottreng@users.noreply.github.com>
2021-11-20 19:56:19 -0500

    Update basicRESTapi.py

--- basicRESTapi.py
+++ basicRESTapi.py
@@ -25,4 +25,4 @@ if __name__ == '__main__':
     logging.basicConfig(filename='serverRun.log', level=logging.INFO)
     logger = logging.getLogger(__name__)
     print('basic REST api enabled on localhost port 8080')
-    app.run(debug=True, host='127.0.0.1', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
